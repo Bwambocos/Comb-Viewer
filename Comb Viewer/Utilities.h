189e5dec75981ce481ec2f6897c08fe29645ebbf
Bwambocos <mijyumaru2525@outlook.jp>
2018-09-06 21:49:55 +0900

    Utilities: 時間計測クラス 実装

--- Comb Viewer/Utilities.h
+++ Comb Viewer/Utilities.h
@@ -5,3 +5,15 @@
 
 // フルスクリーン表示化
 void turnToFullScreen();
+
+// 時間計測クラス
+class myTimer
+{
+private:
+	uint64 nowMilliSec, startMilliSec;
+
+public:
+	void update();
+	uint64 query();
+	void reset();
+};
