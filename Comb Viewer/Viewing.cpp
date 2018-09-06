2e2640c39c7dba029b9c7164ff5a8e0aee0a0dc5
Bwambocos <mijyumaru2525@outlook.jp>
2018-09-06 17:55:06 +0900

    Viewing: 画像データ読み込み 作成
    ・データの読み込みを作成

--- Comb Viewer/Viewing.cpp
+++ Comb Viewer/Viewing.cpp
@@ -5,7 +5,27 @@
 // ‰æ‘œ‰{—— ‰Šú‰»
 Viewing::Viewing(const InitData& init) :IScene(init)
 {
+	TextReader listReader(U"data//work_list.txt");
+	String folderName;
+	while (listReader.readLine(folderName))
+	{
+		work tempWork;
+		tempWork.workImage = Texture(U"data//" + folderName + U"//Image.png");
+		TextReader descriptionReader(U"data//" + folderName + U"//Description.txt");
+		descriptionReader.readLine(tempWork.titleName);
+		descriptionReader.readLine(tempWork.makerName);
+		String tempDescription;
+		while (descriptionReader.readLine(tempDescription)) tempWork.description += tempDescription;
+		tempWork.x = Window::Width() / 2;
+		tempWork.y = Window::Height() / 2;
 
+		if (tempWork.workImage.height() >= tempWork.workImage.width()) tempWork.ragRatio = (double)Window::Height() / tempWork.workImage.height();
+		else tempWork.ragRatio = (double)Window::Width() / tempWork.workImage.width();
+		works.push_back(tempWork);
+	}
+	titleFont = Font(48);
+	makerFont = Font(36);
+	descriptionFont = Font(28);
 }
 
 // ‰æ‘œ‰{—— XV
@@ -17,5 +37,8 @@ void Viewing::update()
 // ‰æ‘œ‰{—— •`‰æ
 void Viewing::draw() const
 {
-
+	for (auto work : works)
+	{
+		work.workImage.scaled(work.ragRatio).drawAt(work.x, work.y);
+	}
 }
--- Comb Viewer/Viewing.h
+++ Comb Viewer/Viewing.h
@@ -3,10 +3,21 @@
 #include <Siv3D.hpp>
 #include <HamFramework.hpp>
 
+// \‘¢‘Ì
+struct work
+{
+	Texture workImage;
+	String titleName, makerName, description;
+	int32 x, y;
+	double ragRatio;
+};
+
 // ‰æ‘œ‰{——
 class Viewing :public MyApp::Scene
 {
 private:
+	Array<work>works;
+	Font titleFont, makerFont, descriptionFont;
 
 public:
 	Viewing(const InitData& init);
