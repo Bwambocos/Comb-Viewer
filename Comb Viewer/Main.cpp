7c30d8136c3cd10b6898e3edfb97c6060c7155c1
Bwambocos <mijyumaru2525@outlook.jp>
2018-09-06 21:32:05 +0900

    develop: デバッグ用機能 追加

--- Comb Viewer/Main.cpp
+++ Comb Viewer/Main.cpp
@@ -8,7 +8,11 @@ void Main()
 	// ウィンドウ関連 初期化
 	Window::SetTitle(U"Comb Viewer " + versionStr);
 	Graphics::SetBackground(Color(32, 32, 32));
+#ifdef _DEBUG
+	Window::Resize(1280, 720);
+#else
 	turnToFullScreen();
+#endif
 
 	// シーン 初期化
 	MyApp sceneMgr;
